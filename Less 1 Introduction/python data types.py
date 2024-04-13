d98185dbb76297cfa2882ae6723d3bab66c547af
Debela394 <debela394@gmail.com>
2024-04-13 12:59:33 +0300

    Built in data types

---
+++ Less 1 Introduction/python data types.py
@@ -0,0 +1,14 @@
+print("These are python built in data typaes")
+
+print('')
+
+""""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
