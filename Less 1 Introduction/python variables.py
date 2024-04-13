d2a4765d5c8ecdb5d85b4e1b16cf3d8166ed4454
Debela394 <debela394@gmail.com>
2024-04-13 14:23:41 +0300

    Assign Variables to multiple and and single values

--- Less 1 Introduction/python variables.py
+++ Less 1 Introduction/python variables.py
@@ -9,3 +9,25 @@ print(MyVariableName)
 print('')
 print(_myVariableName)
 print('')
+
+#Assign many values to multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print(' ')
+
+#Assign one values to multiple variables
+x = y = z = "Banana"
+print(x)
+print(y)
+print(z)
+print('')
+
+#Unpack a collection
+fruits = ["Banana", "Apple", "Cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print('')
